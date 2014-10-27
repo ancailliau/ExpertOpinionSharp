9af2f800e71dee41d825a8050676930a1e0d74d4
Antoine Cailliau <a.cailliau@ac-graphic.net>
2014-10-27 10:42:29 +0100

    Initial commit

---
+++ ExpertOpinionSharp/Expert.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpertOpinionModelling
+{
+    /// <summary>
+    /// Models an expert.
+    /// </summary>
+    class Expert {
+
+        /// <summary>
+        /// Gets the name of the expert
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the estimates for all variables
+        /// </summary>
+        /// <value>The estimates.</value>
+        public IDictionary<ExpertVariable, ExpertOpinion> Estimates {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpertOpinionModelling.Expert"/> class.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        /// <param name="estimates">Estimates.</param>
+        public Expert(string name, IEnumerable<ExpertOpinion> estimates)
+        {
+            this.Name = name;
+            this.Estimates = new Dictionary<ExpertVariable, ExpertOpinion> ();
+            foreach (var e in estimates) {
+                Estimates.Add (e.Variable, e);
+            }
+        }
+
+    }
+}
---
+++ ExpertOpinionSharp/ExpertOpinion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpertOpinionModelling
+{
+    /// <summary>
+    /// Models the opinion of an expert for a given variable. 
+    /// </summary>
+    class ExpertOpinion {
+
+        /// <summary>
+        /// Gets the variable.
+        /// </summary>
+        /// <value>The variable.</value>
+        public ExpertVariable Variable {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the estimates.
+        /// </summary>
+        /// <value>The estimates.</value>
+        public List<double> Estimates {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpertOpinionModelling.ExpertOpinion"/> class.
+        /// </summary>
+        /// <param name="variable">Variable.</param>
+        /// <param name="estimates">Estimates.</param>
+        public ExpertOpinion(ExpertVariable variable, IEnumerable<double> estimates)
+        {
+            this.Variable = variable;
+            this.Estimates = new List<double> (estimates);
+        }
+    }
+}
+
